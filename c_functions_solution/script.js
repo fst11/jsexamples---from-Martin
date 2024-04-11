f478ef9aa1c5ea829b9c302d31a6eec57db58e98
Francis Seo Tho <fst11@yahoo.com>
2024-04-11 12:00:02 +0800

    solution folder

---
+++ c_functions_solution/script.js
@@ -0,0 +1,40 @@
+
+/* 
+  ftnSalutation is a conventional JS function
+  it returns an array of strings where each
+  element (or index) of the array consists
+  of the salutations to pupulate inputSelect
+  found in index.html
+*/
+
+/* 
+  ftnSalutation written as an ARROW function
+  it uses the passed-in parameters to form
+  a greeting to be returned to the function that
+  calls it
+*/
+function ftnSalutation() {
+  //  implement your statement(s) here
+  return new Array("Mr.", "Mrs.", "Ms.", "Dr.");
+}
+
+const ftnGreeting = (saluation, user) => {
+  // write msg here
+  const msg = "Hi " + saluation + " " + user + ",";
+  return msg;
+}
+
+/*
+Complete the challenge statement below
+Create the function validateInputs below
+Hint: 
+- validateInputs accepts two arguments (choice, name)
+- name should include a default value name=""
+- validateInputs returns false if either choice or name is incorrectly filled
+- validateInputs returns true if both choice and name are correctly filled
+*/
+
+function validateInputs(saluation, user){
+var msg = "hi, " + saluation + " " + user;
+  return msg;
+}
